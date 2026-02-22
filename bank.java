f674f67d1ef9ae8b815dbc7d1e2b3ff8dce41c5b
cepopf <punnama.loc@student.mahidol.ac.th>
2026-02-23 01:13:20 +0700

    add some set and get

--- bank.java
+++ bank.java
@@ -1,24 +1,37 @@
-public class bank extends bankAccount{
-    //variable
+public class bank extends bankAccount {
+    // variable
     private String bankName = null;
     private Double transferFee = null;
 
-    //constructor
-    public bank(String accName, String accountID, Double balance, String bankName, Double transferFee){
+    // constructor
+    public bank(String accName, String accountID, Double balance, String bankName, Double transferFee) {
         super(accName, accountID, balance);
         this.bankName = bankName;
         this.transferFee = transferFee;
     }
 
-    //set
+    // set
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
 
+    public void setTransferFee(Double transferFee) {
+        this.transferFee = transferFee;
+    }
 
-    //get
+    // get
+    public String getBankName() {
+        return this.bankName;
+    }
 
+    public Double getTransferFee() {
+        return this.transferFee;
+    }
 
-    //method
-    public boolean checkBank(String bankName){
-        //check if target account id the same bank or not
+    // method
+    public boolean checkBank(String bankName) {
+        // check if target account id the same bank or not
+        
         return true;
     }
 }
--- bankAccount.java
+++ bankAccount.java
@@ -6,19 +6,55 @@ public abstract class bankAccount{
     
     //constructor
     public bankAccount(String accName, String accountID, Double balance){
+        this.accName = accName;
+        this.accountID = accountID;
+        this.balance = balance;
     }
 
     //set
+    public void setAccName(String accName) {
+        this.accName = accName;
+    }
 
+    public void setAccountID(String accountID) {
+        this.accountID = accountID;
+    }
+
+    public void setBalance(Double balance) {
+        if (balance >= 0) {
+            this.balance = balance;
+        }
+    }
     
     //get
-    
+    public String getAccName() {
+        return this.accName;
+    }
+
+    public String getAccountID() {
+        return this.accountID;
+    }
+
+    public Double getBalance() {
+        return this.balance;
+    }
 
     //method
     public void withdraw(Double amount) {
         //withdraw money from account
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Invalid withdraw amount.");
+        }
     }
     public void deposit(Double amount){
         //deposit money to account
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+
     }
 }
